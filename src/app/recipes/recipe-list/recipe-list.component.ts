9227abd1cc3e4f80221895fb33c79e2e1a8cfd71
Michal Czyzowicz <czyzowicz.m@gmail.com>
2020-07-06 13:52:06 +0200

    add recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test  recipe', 
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sapien tellus, pharetra non tincidunt non, accumsan finibus erat.',
+    'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
+  ];
 
   constructor() { }
 

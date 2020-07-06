02cffb7f8bcb113846e91dc8bfe89a4dcbbc8094
Michal Czyzowicz <czyzowicz.m@gmail.com>
2020-07-06 14:32:28 +0200

    add example of recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test  recipe', 
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sapien tellus, pharetra non tincidunt non, accumsan finibus erat.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
   ];
 

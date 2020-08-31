94f9387fd86eae8b9e29e64e4c5483cb0a2030f5
Michal Czyzowicz <czyzowicz.m@gmail.com>
2020-08-31 21:54:07 +0200

    next commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,9 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test  recipe', 
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg'),
+    new Recipe('A test  recipe', 
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
   ];
 

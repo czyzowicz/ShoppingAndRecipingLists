bdea12b33364f3bb441af7c71d67e9e238f9b58e
Michal Czyzowicz <czyzowicz.m@gmail.com>
2020-10-16 19:00:46 +0200

    add roating for recipe and shopping list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  ifRecipesComponent: boolean;
+  ifShopingListComponent: boolean;
+
+  navigateRecipes(ifRecipes: boolean) {
+    this.ifRecipesComponent = ifRecipes;
+    this.ifShopingListComponent = !ifRecipes;
+  }
+
+  navigateShoppingList(ifShoppingList: boolean) {
+    this.ifShopingListComponent = ifShoppingList;
+    this.ifRecipesComponent = !ifShoppingList;
+  }
+
+
+
+}
--- src/app/headeer/header.component.ts
+++ src/app/headeer/header.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() selectedRecipes = new EventEmitter<boolean>();
+    @Output() selectedShoppingList = new EventEmitter<boolean>();
 
+    changeForRecipies() {
+        this.selectedRecipes.emit(true);
+    }
+    changeForShoppingList() {
+        this.selectedShoppingList.emit(true);
+    }
 }
